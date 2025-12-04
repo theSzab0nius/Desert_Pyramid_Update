8e225db3c5d952afc4f568dde5abc750ed84642b
theSzab0nius <rolad46official@gmail.com>
2025-12-04 18:11:20 +0100

    Initial commit

---
+++ src/main/java/net/mossclock/desert_pyramid_update/Desert_pyramid_client.java
@@ -0,0 +1,10 @@
+package net.mossclock.desert_pyramid_update;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Desert_pyramid_client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mossclock/desert_pyramid_update/Desert_pyramid_updateDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mossclock.desert_pyramid_update;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Desert_pyramid_updateDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
