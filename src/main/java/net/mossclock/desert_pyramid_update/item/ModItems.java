8ca2b846119af06d82ddb2dc653d1f9512bac36a
theSzab0nius <rolad46official@gmail.com>
2025-12-04 19:24:42 +0100

    Added rusted_khopesh item v1.0

--- src/main/java/net/mossclock/desert_pyramid_update/item/ModItems.java
+++ src/main/java/net/mossclock/desert_pyramid_update/item/ModItems.java
@@ -1,12 +1,16 @@
 package net.mossclock.desert_pyramid_update.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.mossclock.desert_pyramid_update.Desert_pyramid_update;
 
 public class ModItems {
+
+    public static final Item RUSTED_KHOPESH = registerItem("rusted_khopesh", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
 
@@ -15,5 +19,9 @@ public class ModItems {
 
     public static void registerModItems () {
         Desert_pyramid_update.LOGGER.info("Registering modItems for " + Desert_pyramid_update.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(RUSTED_KHOPESH);
+        });
     }
 }
