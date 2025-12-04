fb8291b726120ce29c54282c07d6c6040d5db1ab
theSzab0nius <rolad46official@gmail.com>
2025-12-04 18:19:38 +0100

    Added ModItems

---
+++ src/main/java/net/mossclock/desert_pyramid_update/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.mossclock.desert_pyramid_update.item;
+
+import net.mossclock.desert_pyramid_update.Desert_pyramid_update;
+
+public class ModItems {
+
+    public static void registerModItems () {
+        Desert_pyramid_update.LOGGER.info("Registering modItems for " + Desert_pyramid_update.MOD_ID);
+    }
+}
